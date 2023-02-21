a3b683aaf92a73973542827fa295e6a791032daf
Jack Lu <jacklu@jacklu.me>
2023-02-21 17:54:30 +0800

    +add, remove

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # wechaty-dynamic-client
 
 [`wechaty-dynamic`](https://github.com/panlina/wechaty-dynamic) client.
+
+## Usage
+
+```shell
+% node index.js http://localhost:3000
+> add a /a.js
+added a
+> remove a
+removed a
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+var readline = require('readline');
+var shellQuote = require('shell-quote');
+var { default: axios } = require('axios');
+
+var [, , endpoint] = process.argv;
+
+(async function () {
+	var rl = readline.createInterface(process.stdin, process.stdout);
+	rl.prompt();
+	for await (var line of rl) {
+		if (line == 'exit') break;
+		var args = shellQuote.parse(line);
+		if (args[0] == 'add' && args.length == 3) {
+			var [, name, module] = args;
+			try {
+				await axios.put(`${endpoint}/plugin/${name}`, { module: module }, { headers: { "Content-Type": "application/json" } });
+				console.log(`added ${name}`);
+			} catch (e) {
+				console.error("error");
+			}
+		}
+		else if (args[0] == 'remove' && args.length == 2) {
+			var [, name] = args;
+			try {
+				await axios.delete(`${endpoint}/plugin/${name}`);
+				console.log(`removed ${name}`);
+			} catch (e) {
+				console.error("error");
+			}
+		}
+		else
+			console.log("syntax error");
+		rl.prompt();
+	}
+	rl.close();
+})();
